4ba0c2041c4b98cc24f2e4f63f1f9673fb250482
MichaelShawUK <MichaelShawUK@gmail.com>
2023-05-04 16:24:15 +0100

    Log user in on successful registration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
+const handleRegister = require("../controllers/handleRegister");
+const handleLogin = require("../controllers/handleLogin");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.status(200).json({ message: "OdinBook API" });
 });
+
+router.post("/register", handleRegister, handleLogin);
+router.post("/login", handleLogin);
 
 module.exports = router;
