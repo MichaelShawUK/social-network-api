75f16bf2b2fe47f60819a631c8073af3284a7e77
MichaelShawUK <MichaelShawUK@gmail.com>
2023-05-04 14:12:02 +0100

    Connect to local database

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
+
+async function connectDb() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/odinbook");
+}
+connectDb().catch((err) => console.log(err));
 
 var app = express();
 
