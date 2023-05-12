56846bb2fc7a3f80f98c9d6075d82d1dc24957cd
MichaelShawUK <MichaelShawUK@gmail.com>
2023-05-12 16:54:07 +0100

    Configure CORS to allow requests from development react client

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 
@@ -16,6 +17,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:3001" }));
 
 app.use("/", indexRouter);
 
