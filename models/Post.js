7a63d05b6b669b4fc7affda36c1e2bb9ac03bf2a
MichaelShawUK <MichaelShawUK@gmail.com>
2023-06-08 12:43:06 +0100

    Add 1000 max length validator to Post text property

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema(
   {
-    text: { type: String },
+    text: { type: String, maxLength: 1000 },
     image: { type: String },
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
     likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
