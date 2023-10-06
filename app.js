1f01916c3826ff91ec439b875fcbd2a01827a7b3
MichaelShawUK <MichaelShawUK@gmail.com>
2023-10-06 14:13:33 +0100

    Set CORS origin to frontend url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(cors({ origin: "https://odinbook-production-a5e1.up.railway.app" }));
-app.use(cors());
+app.use(cors({ origin: "https://odinbook-production-a5e1.up.railway.app/" }));
 
 app.use("/", indexRouter);
 
